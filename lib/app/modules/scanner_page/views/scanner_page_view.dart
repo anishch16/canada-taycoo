7675091220bac52dea82d4cfb58148452b0f3971
chaulagainanish16 <anish.chaulagain@ankaek.com>
2024-10-06 10:13:49 +0545

    added pulsuator

--- lib/app/modules/scanner_page/views/scanner_page_view.dart
+++ lib/app/modules/scanner_page/views/scanner_page_view.dart
@@ -1,12 +1,10 @@
 import 'dart:math';
-import 'package:barcode/barcode.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:pulsator/pulsator.dart';
 
 // import 'package:mobile_scanner/mobile_scanner.dart';
 import '../../../resources/text_style.dart';
-import '../../../resources/utils.dart';
 import '../../../routes/app_pages.dart';
 import '../../home/views/order_card.dart';
 import '../controllers/scanner_page_controller.dart';
@@ -17,17 +15,18 @@ class ScannerPageView extends GetView<ScannerPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: GestureDetector(
         onTap: () => controller.scanBarcode(),
-        child: Container(
-          padding: const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 16),
-          decoration: const BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
-          child: const Icon(
-            Icons.qr_code_2,
-            size: 34,
-            color: Colors.white,
-          ),
+        child: const PulseIcon(
+          icon: Icons.qr_code_scanner,
+          pulseColor: Colors.orange,
+          iconColor: Colors.white,
+          iconSize: 34,
+          innerSize: 64,
+          pulseSize: 116,
+          pulseCount: 1,
+          pulseDuration: Duration(seconds: 4),
         ),
       ),
       appBar: AppBar(
@@ -52,89 +51,90 @@ class ScannerPageView extends GetView<ScannerPageController> {
         ),
       ),
       body: Obx(
-        () => Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-              child: Row(
-                children: [
-                  Flexible(
-                    child: SizedBox(
-                      height: 50,
-                      child: TextFormField(
-                        cursorColor: Colors.orange,
-                        decoration: InputDecoration(
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            suffixIcon: const Padding(
-                              padding: EdgeInsets.only(right: 8.0),
-                              child: Icon(
-                                Icons.search,
-                                color: Colors.orange,
+        () => SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: SizedBox(
+                        height: 50,
+                        child: TextFormField(
+                          cursorColor: Colors.orange,
+                          decoration: InputDecoration(
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
+                              suffixIcon: const Padding(
+                                padding: EdgeInsets.only(right: 8.0),
+                                child: Icon(
+                                  Icons.search,
+                                  color: Colors.orange,
+                                ),
                               ),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(color: Colors.orange),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(color: Colors.orange),
-                            ),
-                            errorBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(color: Colors.orange),
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                              borderSide: const BorderSide(color: Colors.orange),
-                            ),
-                            labelText: "Order ID",
-                            labelStyle: AppTextTheme.textTheme.titleLarge?.copyWith(color: Colors.orange)),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Colors.orange),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Colors.orange),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Colors.orange),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: const BorderSide(color: Colors.orange),
+                              ),
+                              labelText: "Order ID",
+                              labelStyle: AppTextTheme.textTheme.titleLarge?.copyWith(color: Colors.orange)),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Container(
-                    height: 50,
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    child: Center(
-                        child: Text(
-                      "Search",
-                      style: AppTextTheme.textTheme.titleLarge?.copyWith(color: Colors.white),
-                    )),
-                  )
-                ],
+                    const SizedBox(width: 16.0),
+                    Container(
+                      height: 50,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Center(
+                          child: Text(
+                        "Search",
+                        style: AppTextTheme.textTheme.titleLarge?.copyWith(color: Colors.white),
+                      )),
+                    )
+                  ],
+                ),
               ),
-            ),
-            controller.scanResult.value == null
-                ? Center(
-                    child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 100),
-                        Image.asset(
-                          "assets/png/scanning.png",
-                          height: 200,
-                        ),
-                        Text(
-                          "Use Search or Scanner to get the ordered card details",
-                          style: AppTextTheme.textTheme.displayMedium?.copyWith(color: Colors.orange),
-                          textAlign: TextAlign.center,
-                          maxLines: 3,
-                        )
-                      ],
-                    ),
-                  ))
-                : Expanded(
-                  child: ListView.builder(
+              controller.scanResult.value == null
+                  ? Center(
+                      child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 100),
+                          Image.asset(
+                            "assets/png/scanning.png",
+                            height: 200,
+                          ),
+                          Text(
+                            "Use Search or Scanner to get the ordered card details",
+                            style: AppTextTheme.textTheme.displayMedium?.copyWith(color: Colors.orange),
+                            textAlign: TextAlign.center,
+                            maxLines: 3,
+                          )
+                        ],
+                      ),
+                    ))
+                  : ListView.builder(
                       shrinkWrap: true,
                       itemCount: 1,
+                      physics: const NeverScrollableScrollPhysics(),
                       itemBuilder: (context, index) {
                         return Container(
                           margin: const EdgeInsets.symmetric(
@@ -280,9 +280,9 @@ class ScannerPageView extends GetView<ScannerPageController> {
                       //     controller.autoEnableFlash.value = checked!;
                       //   },
                       // ),
-                    ),
-                )
-          ],
+                    )
+            ],
+          ),
         ),
       ),
     );
@@ -427,170 +427,3 @@ class ScannerPageView extends GetView<ScannerPageController> {
 //   }
 // }
 
-class QrScannerOverlayShape extends ShapeBorder {
-  QrScannerOverlayShape({
-    this.borderColor = Colors.red,
-    this.borderWidth = 3.0,
-    this.overlayColor = const Color.fromRGBO(0, 0, 0, 80),
-    this.borderRadius = 0,
-    this.borderLength = 40,
-    double? cutOutSize,
-    double? cutOutWidth,
-    double? cutOutHeight,
-    this.cutOutBottomOffset = 0,
-  })  : cutOutWidth = cutOutWidth ?? cutOutSize ?? 250,
-        cutOutHeight = cutOutHeight ?? cutOutSize ?? 250 {
-    assert(
-      borderLength <= min(this.cutOutWidth, this.cutOutHeight) / 2 + borderWidth * 2,
-      "Border can't be larger than ${min(this.cutOutWidth, this.cutOutHeight) / 2 + borderWidth * 2}",
-    );
-    assert((cutOutWidth == null && cutOutHeight == null) || (cutOutSize == null && cutOutWidth != null && cutOutHeight != null),
-        'Use only cutOutWidth and cutOutHeight or only cutOutSize');
-  }
-
-  final Color borderColor;
-  final double borderWidth;
-  final Color overlayColor;
-  final double borderRadius;
-  final double borderLength;
-  final double cutOutWidth;
-  final double cutOutHeight;
-  final double cutOutBottomOffset;
-
-  @override
-  EdgeInsetsGeometry get dimensions => const EdgeInsets.all(10);
-
-  @override
-  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
-    return Path()
-      ..fillType = PathFillType.evenOdd
-      ..addPath(getOuterPath(rect), Offset.zero);
-  }
-
-  @override
-  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
-    Path getLeftTopPath(Rect rect) {
-      return Path()
-        ..moveTo(rect.left, rect.bottom)
-        ..lineTo(rect.left, rect.top)
-        ..lineTo(rect.right, rect.top);
-    }
-
-    return getLeftTopPath(rect)
-      ..lineTo(
-        rect.right,
-        rect.bottom,
-      )
-      ..lineTo(
-        rect.left,
-        rect.bottom,
-      )
-      ..lineTo(
-        rect.left,
-        rect.top,
-      );
-  }
-
-  @override
-  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
-    final width = rect.width;
-    final borderWidthSize = width / 2;
-    final height = rect.height;
-    final borderOffset = borderWidth / 2;
-    final mBorderLength = borderLength > min(cutOutHeight, cutOutHeight) / 2 + borderWidth * 2 ? borderWidthSize / 2 : borderLength;
-    final mCutOutWidth = cutOutWidth < width ? cutOutWidth : width - borderOffset;
-    final mCutOutHeight = cutOutHeight < height ? cutOutHeight : height - borderOffset;
-
-    final backgroundPaint = Paint()
-      ..color = overlayColor
-      ..style = PaintingStyle.fill;
-
-    final borderPaint = Paint()
-      ..color = borderColor
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = borderWidth;
-
-    final boxPaint = Paint()
-      ..color = borderColor
-      ..style = PaintingStyle.fill
-      ..blendMode = BlendMode.dstOut;
-
-    final cutOutRect = Rect.fromLTWH(
-      rect.left + width / 2 - mCutOutWidth / 2 + borderOffset,
-      -cutOutBottomOffset + rect.top + height / 2 - mCutOutHeight / 2 + borderOffset,
-      mCutOutWidth - borderOffset * 2,
-      mCutOutHeight - borderOffset * 2,
-    );
-
-    canvas
-      ..saveLayer(
-        rect,
-        backgroundPaint,
-      )
-      ..drawRect(
-        rect,
-        backgroundPaint,
-      )
-      // Draw top right corner
-      ..drawRRect(
-        RRect.fromLTRBAndCorners(
-          cutOutRect.right - mBorderLength,
-          cutOutRect.top,
-          cutOutRect.right,
-          cutOutRect.top + mBorderLength,
-          topRight: Radius.circular(borderRadius),
-        ),
-        borderPaint,
-      )
-      // Draw top left corner
-      ..drawRRect(
-        RRect.fromLTRBAndCorners(
-          cutOutRect.left,
-          cutOutRect.top,
-          cutOutRect.left + mBorderLength,
-          cutOutRect.top + mBorderLength,
-          topLeft: Radius.circular(borderRadius),
-        ),
-        borderPaint,
-      )
-      // Draw bottom right corner
-      ..drawRRect(
-        RRect.fromLTRBAndCorners(
-          cutOutRect.right - mBorderLength,
-          cutOutRect.bottom - mBorderLength,
-          cutOutRect.right,
-          cutOutRect.bottom,
-          bottomRight: Radius.circular(borderRadius),
-        ),
-        borderPaint,
-      )
-      // Draw bottom left corner
-      ..drawRRect(
-        RRect.fromLTRBAndCorners(
-          cutOutRect.left,
-          cutOutRect.bottom - mBorderLength,
-          cutOutRect.left + mBorderLength,
-          cutOutRect.bottom,
-          bottomLeft: Radius.circular(borderRadius),
-        ),
-        borderPaint,
-      )
-      ..drawRRect(
-        RRect.fromRectAndRadius(
-          cutOutRect,
-          Radius.circular(borderRadius),
-        ),
-        boxPaint,
-      )
-      ..restore();
-  }
-
-  @override
-  ShapeBorder scale(double t) {
-    return QrScannerOverlayShape(
-      borderColor: borderColor,
-      borderWidth: borderWidth,
-      overlayColor: overlayColor,
-    );
-  }
-}
